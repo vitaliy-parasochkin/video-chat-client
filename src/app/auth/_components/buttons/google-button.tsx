62f6222bc11c5302e2fd7e7a3f6b5dc11062bf92
parasochkinvitalii <parasochinvitaliy@gmail.com>
2024-08-16 22:12:06 +0300

    fix google button name

--- src/app/auth/_components/buttons/google-button.tsx
+++ src/app/auth/_components/buttons/google-button.tsx
@@ -15,7 +15,7 @@ function GoogleButton({callbackUrl}: GithubButtonProps) {
       variant="ghost"
       className="flex cursor-pointer items-center justify-center gap-x-3 rounded-xl bg-slate-100 px-16 py-5 dark:bg-slate-800"
     >
-      Continue with github <FcGoogle className="h-6 w-6" />
+      Continue with google <FcGoogle className="h-6 w-6" />
     </Button>
   );
 }
